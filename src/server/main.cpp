05cb15c94c5071ab04fd4cd2b78d42b7dd3ce283
Alberto Fustinoni <alberto@aftnet.net>
2023-07-10 14:17:00 +0900

    Logic

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -3,35 +3,22 @@
 #include <string>
 
 #include "globals.hpp"
-#include "asio.hpp"
+#include "mongoose.h"
+
+static void fn(struct mg_connection* c, int ev, void* ev_data, void* fn_data)
+{
+    if (ev == MG_EV_HTTP_MSG)
+    {
+        mg_http_reply(c, 200, "", "{%m:%d}\n", MG_ESC("status"), 1);
+    }
+}
 
 int main()
 {
-    using asio::ip::tcp;
-
-    try
-    {
-        asio::io_context io_context;
-
-        std::cout << "Awaiting client connection" << std::endl;
-
-        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v6(), rinstaller::common::PORT_NUMBER));
-        acceptor.set_option(asio::detail::socket_option::integer<SOL_SOCKET, SO_RCVTIMEO>{ 200 });
-        tcp::socket socket(io_context);
-        acceptor.accept(socket);
-        std::cout << "Client connected" << std::endl;
-
-        asio::error_code ignored_error;
-        std::cout << "Sending payload" << std::endl;
-        asio::write(socket, asio::buffer("Hello there!"), ignored_error);
-        std::cout << "Sent" << std::endl;
-
-        std::cin.get();
-    }
-    catch (std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
+    struct mg_mgr mgr;
+    mg_mgr_init(&mgr);                                      // Init manager
+    mg_http_listen(&mgr, "http://0.0.0.0:8000", fn, NULL);  // Setup listener
+    for (;;) mg_mgr_poll(&mgr, 1000);                       // Infinite event loop
 
     return 0;
 }
