ce5b763c63d15efaa1597d964f6203b4042ad6e4
Alberto Fustinoni <alberto@aftnet.net>
2024-01-18 18:08:10 +0900

    Logic

--- src/common/certstore.cpp
+++ src/common/certstore.cpp
@@ -4,6 +4,9 @@
 #include "mbedtls/x509_crt.h"
 #include "mbedtls/entropy.h"
 #include <mbedtls/ctr_drbg.h>
+#include <array>
+#include <string>
+#include <iostream>
 
 void CertStore::Init()
 {
@@ -12,7 +15,7 @@ void CertStore::Init()
 
 	mbedtls_entropy_init(&entropy);
 	mbedtls_ctr_drbg_init(&ctr_drbg);
-    if (mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, (const unsigned char*)"RANDOM_GEN", 10) != 0)
+    if (mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, (const unsigned char*)"RANDOM_GEN", 10))
     {
         throw;
     }
@@ -21,7 +24,7 @@ void CertStore::Init()
 
     mbedtls_pk_context key;
     mbedtls_pk_init(&key);
-    if (mbedtls_pk_setup(&key, mbedtls_pk_info_from_type(MBEDTLS_PK_RSA)) != 0)
+    if (mbedtls_pk_setup(&key, mbedtls_pk_info_from_type(MBEDTLS_PK_RSA)))
     {
         throw;
     }
@@ -30,6 +33,19 @@ void CertStore::Init()
     {
         throw;
     }
+
+    std::array<unsigned char, 8192> privkey, pubkey;
+    if (mbedtls_pk_write_key_pem(&key, privkey.data(), privkey.size()))
+    {
+        throw;
+    }
+    if (mbedtls_pk_write_pubkey_pem(&key, pubkey.data(), pubkey.size()))
+    {
+        throw;
+    }
+
+    std::cout << privkey.data();
+    std::cout << pubkey.data();
 
 	mbedtls_x509write_cert cert;
 	mbedtls_x509write_crt_init(&cert);
