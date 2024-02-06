9ce9f99dfd8bc634618cfd190d4e2be599749400
alberto <alberto@aftnet.net>
2024-02-06 23:38:29 +0900

    Logic

--- src/common/include/mbedtlsmem.h
+++ src/common/include/mbedtlsmem.h
@@ -7,7 +7,22 @@ struct mbedtls_ssl_config;
 struct mbedtls_x509write_cert;
 struct mbedtls_x509_crt;
 
-class MbedtlsCtrDrdbgContext
+namespace RInstaller
 {
-	mbedtls_ctr_drbg_context* p;
-};
+	namespace MbedtlsMem
+	{
+		class CtrDrdbgContext
+		{
+		private:
+			mbedtls_ctr_drbg_context* p;
+
+		public:
+			CtrDrdbgContext();
+			~CtrDrdbgContext();
+			CtrDrdbgContext(const CtrDrdbgContext&) = delete;
+			CtrDrdbgContext(CtrDrdbgContext&& other) noexcept;
+			inline mbedtls_ctr_drbg_context* get() { return p; }
+		};
+	}
+}
+
--- src/common/mbedtlsmem.cpp
+++ src/common/mbedtlsmem.cpp
@@ -1,1 +1,25 @@
 #include "mbedtlsmem.h"
+
+#include "mbedtls/ctr_drbg.h"
+#include "mbedtls/entropy.h"
+
+using namespace RInstaller::MbedtlsMem;
+
+CtrDrdbgContext::CtrDrdbgContext() :
+	p(new mbedtls_ctr_drbg_context)
+{
+	mbedtls_ctr_drbg_init(p);
+}
+
+CtrDrdbgContext::~CtrDrdbgContext()
+{
+	mbedtls_ctr_drbg_free(p);
+	delete p;
+}
+
+CtrDrdbgContext::CtrDrdbgContext(CtrDrdbgContext&& other) noexcept
+{
+	auto tmp = p;
+	p = other.p;
+	other.p = tmp;
+}
